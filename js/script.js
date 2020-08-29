1b9f8b19f6c56e9e140bce3a7db2b24d0292700d
Anna Kaser <kaserann96@gmail.com>
2020-08-28 21:12:54 -0600

    moment color code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
-var currentDate = new Date();
+var currentDate = moment().format('MMMM Do YYYY');
 
-var month = currentDate.getMonth()+1;
-var day = currentDate.getDate();
-var year = currentDate.getFullYear();
+console.log(currentDate);
+$('#currentDay').text(currentDate);
 
-var output =  (day<10 ? '0' : '') + day + '/' + (month<10 ? '0' : '') + month + '/' + year;
-console.log(output);
+$('.edit-task').on("click", "p", function () {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $('<textarea>')
+        .addClass("text-box")
+        .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger('focus');
+});
 
-// var output = document.getElementById('currentDay')
+$(".edit-task").on('blur', 'textarea', function () {
+    var text = $(this)
+        .val()
+        .trim();
+
+    var taskP = $("<p>")
+        .addClass("text-box")
+        .text(text);
+
+    $(this).replaceWith(taskP);
+})
+
+var hour = moment().format("HH");
+console.log(hour);
+
+var pastthings = $('.hour-time')
+console.log(pastthings)
+
+pastthings.each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+    let timeBlock = parseInt($(this).text());
+    if(timeBlock < hour)
+    {
+        console.log("Should be past");
+        $(this).parent().addClass("past");
+    }
+    else if(timeBlock > hour)
+    {
+        console.log("Should be future");
+        $(this).parent().addClass("future");
+    }
+    else
+    {
+        console.log("Right now!");
+        $(this).parent().addClass("present");
+    }
+  });
